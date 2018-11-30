1b2d264a12086af9badcbaa42aa7d6579dd9d083
lichongbin <lichongbin2004@126.com>
2018-11-30 21:15:05 +0800

    add some comments

--- chapter01/ch01_07_06.m
+++ chapter01/ch01_07_06.m
@@ -1,2 +1,4 @@
+% this is my first experience about matlab
+% read an image from disk
 f = imread('Fig0101.tif');
 imshow(f);
---
+++ chapter01/sinfun2.m
@@ -0,0 +1,7 @@
+% an improved implementation by preallocation
+function y = sinfun2(M)
+x = 0:M-1
+y = zeros(1, numel(x)); % preallocation
+for k = 1:numel(x)
+    y(k) = sin(x(k) / (100 * pi));
+end
